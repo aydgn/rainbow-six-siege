da249e35521bb68ffd9996b7ede2d256d359db00
Aydoğan Gökberk <aydogan.gokberk@gmail.com>
2021-03-03 23:15:44 +0300

    layout capital name fix

--- pages/operators/index.js
+++ pages/operators/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Layout from "../../components/Layout";
+import Layout from "../../components/layout";
 import Op from "../../components/Op";
 
 export const getStaticProps = async () => {
